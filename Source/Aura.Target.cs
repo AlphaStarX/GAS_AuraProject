b631f46a6756fe6a2ca28c7955d2bdb1dcf347b0
bmachhi <bhavin19980630@gmail.com>
2026-08-07 15:04:21 -0400

    Fix UE 5.8 Target rules and enable bOverrideBuildEnvironment

--- Source/Aura.Target.cs
+++ Source/Aura.Target.cs
@@ -8,7 +8,9 @@ public class AuraTarget : TargetRules
 	public AuraTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
+		bOverrideBuildEnvironment = true;
 
 		ExtraModuleNames.AddRange( new string[] { "Aura" } );
 	}
--- Source/AuraEditor.Target.cs
+++ Source/AuraEditor.Target.cs
@@ -8,7 +8,9 @@ public class AuraEditorTarget : TargetRules
 	public AuraEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
+		bOverrideBuildEnvironment = true;
 
 		ExtraModuleNames.AddRange( new string[] { "Aura" } );
 	}
